6714c29df10f1198bc8c005f92ff6a2c6c5a343c
Junior M. Soares <jrmsrs2.0@gmail.com>
2022-12-08 18:15:07 +0000

    Update notFound.js

--- routes/notFound.js
+++ routes/notFound.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 router.use((req, res, next) => {
-    const error = new Error('not found')
-    error.status = 404
-    next(error)
-})
-router.use((error,req,res,next) => {
-    res.status(error.status || 500)
-    res.send({
-        error : {
-            message: error.message
-        }
-    })
-})
+  const error = new Error("not found");
+  error.status = 404;
+  next(error);
+});
+router.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.send({
+    error: {
+      message: error.message,
+    },
+    rotas: ["http://" + req.host + "/user", "http://" + req.host + "/group"],
+  });
+});
 
-module.exports = router
+module.exports = router;
