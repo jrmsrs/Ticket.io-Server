73c879265ecfa5ac0ad2703b81d1541ab80885f5
Junior M. Soares <jrmsrs2.0@gmail.com>
2022-12-30 21:32:40 +0000

    Update package-lock.json and lero.js

--- routes/lero.js
+++ routes/lero.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const { tpLero } = require("lerolero");
 
 router.get("/", (req, res, next) => {
-  return res.send(tpLero);
+  return res.send({
+    tpTitleLero: tpLero.tpTitleLero(),
+    tpDescLero: tpLero.tpDescLero(),
+  });
 });
 
 module.exports = router;
