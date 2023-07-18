102a522623244dfa50f6b82d57dd0ac703315213
junior m. soares <jrmsrs2.0@gmail.com>
2023-07-17 23:24:06 -0300

    ReFaToRaDa: update structure, migrate to yarn, update dependencies

--- src/server.js
+++ src/server.js
@@ -2,4 +2,6 @@ const http = require('http')
 const app = require('./app')
 const port = process.env.PORT || 5000
 const server = http.createServer(app)
-server.listen(port)
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`)
+})
